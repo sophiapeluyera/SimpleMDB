ba781dfdde124cfce865eb0c62a96ad7f4ceb16c
sophiapeluyera <sophiapeluyera@gmail.com>
2025-09-19 19:51:02 -0400

    HTML Templates added

--- src/auth/AuthController.cs
+++ src/auth/AuthController.cs
@@ -14,11 +14,11 @@ namespace SimpleMDB
 
         public async Task LandingPageAGet(HttpListenerRequest req, HttpListenerResponse res, Hashtable options)
         {
-            string html = "Hello World!";
+            string html = HtmlTemplates.Base("SimpleMDB", "Landing Page", "Hello World!");
             byte[] content = Encoding.UTF8.GetBytes(html);
             res.StatusCode = (int)HttpStatusCode.OK;
             res.ContentEncoding = Encoding.UTF8;
-            res.ContentType = "text/plain";
+            res.ContentType = "text/html";
             res.ContentLength64 = content.LongLength;
             await res.OutputStream.WriteAsync(content, 0, content.Length);
             res.Close();
