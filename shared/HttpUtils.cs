9c3a327897ffb02d9f61cf2c93e0f9b44457accc
sophiapeluyera <sophiapeluyera@gmail.com>
2025-09-20 14:20:46 -0400

    HttpUtils added

---
+++ shared/HttpUtils.cs
@@ -0,0 +1,21 @@
+using System.Text;
+using System.Net;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace SimpleMDB;
+
+public class HttpUtils
+{
+    public static async Task Respond(HttpListenerRequest req, HttpListenerResponse res, Hashtable options, int statusCode, string body)
+    {
+        byte[] content = Encoding.UTF8.GetBytes(body);
+
+            res.StatusCode = statusCode;
+            res.ContentEncoding = Encoding.UTF8;
+            res.ContentType = "text/html";
+            res.ContentLength64 = content.LongLength;
+            await res.OutputStream.WriteAsync(content);
+            res.Close();
+    }
+}
--- src/auth/AuthController.cs
+++ src/auth/AuthController.cs
@@ -14,14 +14,8 @@ namespace SimpleMDB
 
         public async Task LandingPageAGet(HttpListenerRequest req, HttpListenerResponse res, Hashtable options)
         {
-            string html = HtmlTemplates.Base("SimpleMDB", "Landing Page", "Hello World!");
-            byte[] content = Encoding.UTF8.GetBytes(html);
-            res.StatusCode = (int)HttpStatusCode.OK;
-            res.ContentEncoding = Encoding.UTF8;
-            res.ContentType = "text/html";
-            res.ContentLength64 = content.LongLength;
-            await res.OutputStream.WriteAsync(content, 0, content.Length);
-            res.Close();
+            string html = HtmlTemplates.Base("SimpleMDB", "Landing Page", "Hello World!2");
+            await HttpUtils.Respond(req, res, options, (int) HttpStatusCode.OK, html);
         }
     }
 }
